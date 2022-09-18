ef40ede1a0b45a4f2c2d2203270906cecab4e7f4
Jackson Abney <hickorysmokedbacon.yt@gmail.com>
2022-09-18 13:06:10 -0800

    first commit

---
+++ fxmanifest.lua
@@ -0,0 +1,16 @@
+fx_version 'cerulean'
+games {'gta5'}
+
+author 'TrinityRP'
+real_name 'Vehicle Purchase Script for TRP'
+description 'meh'
+version '1.0.0'
+
+lua54 'yes'
+
+shared_scripts {'config.lua'}
+
+server_scripts {'server/server.lua'}
+
+client_scripts {'client/client.lua'}
+
